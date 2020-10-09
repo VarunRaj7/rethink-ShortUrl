bac78bd3358b2a6c74d931d2e5acf4f2779b6612
VarunRaj7 <rvarunraj1@gmail.com>
2020-10-09 17:11:53 -0400

    adding package for shorturl

--- backend/src/controllers/v1/index.router.ts
+++ backend/src/controllers/v1/index.router.ts
@@ -1,11 +1,22 @@
 import { Router, Request, Response } from 'express';
+import { customAlphabet } from 'nanoid';
+
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  10
+);
 
 const router: Router = Router();
 
 // Get all feed items
-router.get('getShortUrl/', async (req: Request, res: Response) => {
+router.get('/getShortUrl/', async (req: Request, res: Response) => {
   console.log(`${req.body.url}`);
-  res.send('Hello');
+
+  const id = nanoid();
+
+  console.log(`${id}`);
+
+  res.send(`${id}`);
 });
 
 export const IndexRouter: Router = router;
